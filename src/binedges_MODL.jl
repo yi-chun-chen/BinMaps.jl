c61567e3f4a6c285b9609b7a523b3eed9516293a
Tim Wheeler <timwheeleronline@gmail.com>
2015-01-06 13:37:52 -0800

    set up binedges

--- src/binedges_MODL.jl
+++ src/binedges_MODL.jl
@@ -1,1 +1,15 @@
-blah blah
+export DISCRETIZE_MODL_OPTIMAL
+
+immutable Discretize_MODL_Optimal <: DiscretizatonAlgorithm end
+const DISCRETIZE_MODL_OPTIMAL = Discretize_MODL_Optimal()
+
+function binedges{T<:FloatingPoint, S<:Integer}(
+	                :: Discretize_MODL_Optimal, 
+	continuous      :: AbstractArray{T}, 
+	discrete_target :: AbstractArray{S}
+	)
+
+	@assert(length(continuous) == length(discrete_target))
+	
+	# finish implementing here
+end
